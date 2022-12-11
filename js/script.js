572c2f5f0435883cb9e24cd52737df51b9de042f
Kelviny-Mickael <kelviny.mk27@gmail.com>
2022-12-10 23:41:16 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Seleção de elementos
+
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
+// Funções
+
+// Eventos
+menuBtn.addEventListener("click", (e) => {
+
+  menu.classList.toggle("#active");
+
+})
